1bf64caa37c67e739ba3176354a826c15f8fb87e
ProfLeao <prof.reginaldo.leao@gmail.com>
2023-01-17 14:32:27 -0300

    Adiciona comentarios ao modelo de equacao diferencial.

--- EDOMARCELO/marcelo_solver/edo_marcelo.py
+++ EDOMARCELO/marcelo_solver/edo_marcelo.py
@@ -11,12 +11,19 @@ def edo_marcelo(
     f_rho =densidade
 ):
     def modelo(v_temp, v_z):
+        # Primeira parcela
         parc1 = cfcond_termica(v_temp[0]) *\
              v_temp[1] + v_temp[0]*np.gradient(cfcond_termica(v_temp[0]))
+
+        # Segunda parcela
         parc2 = coef_w * f_rho(v_temp[0]) *\
             (f_cv(v_temp[0]) * v_temp[1] +\
                 v_temp[0] * np.gradient(f_cv(v_temp[0])))
+
+        # Terceira parcela
         parc3 = - coef_h * (v_temp[0] - coef_Tinf)
+
+        # Quarta parcela
         parc4 = np.power(coef_j, 2) * coef_r
 
         return parc1 + parc2 + parc3 + parc4
